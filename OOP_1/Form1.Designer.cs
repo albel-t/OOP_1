baa37efc48bcbd193ac03879db9081ee023b8c9e
albel-t <belovalexnder1@gmail.com>
2026-02-19 00:51:40 +0300

    Добавьте файлы проекта.

---
+++ OOP_1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace OOP_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxZ = new System.Windows.Forms.TextBox();
+            this.buttonСalculate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(27, 42);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(331, 396);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(505, 79);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(473, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "X = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(473, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Y = ";
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(505, 105);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 20);
+            this.textBoxY.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(473, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Z = ";
+            // 
+            // textBoxZ
+            // 
+            this.textBoxZ.Location = new System.Drawing.Point(505, 131);
+            this.textBoxZ.Name = "textBoxZ";
+            this.textBoxZ.Size = new System.Drawing.Size(100, 20);
+            this.textBoxZ.TabIndex = 7;
+            // 
+            // buttonСalculate
+            // 
+            this.buttonСalculate.Location = new System.Drawing.Point(77, 183);
+            this.buttonСalculate.Name = "buttonСalculate";
+            this.buttonСalculate.Size = new System.Drawing.Size(75, 23);
+            this.buttonСalculate.TabIndex = 9;
+            this.buttonСalculate.Text = "Сalculate !";
+            this.buttonСalculate.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(381, 486);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Task 1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.buttonСalculate);
+            this.groupBox2.Controls.Add(this.flowLayoutPanel1);
+            this.groupBox2.Location = new System.Drawing.Point(399, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(381, 486);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Task 2";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(52, 55);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 94);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(133, 294);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "f(x, y, z) = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 510);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxZ);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxZ;
+        private System.Windows.Forms.Button buttonСalculate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ OOP_1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace OOP_1
+{
+    public partial class Form1 : Form
+    {
+        Random random;
+        public Form1()
+        {
+            int a = 0, b = 0, c = 0;
+            double ad = 0, bd = 0, cd = 0;
+            InitializeComponent();
+
+            this.buttonСalculate.Click += new System.EventHandler(this.buttonСalculate_Click);
+            
+            random = new Random();
+
+            rand(ref a, ref b, ref c);
+            richTextBox1.AppendText($"{a}-{b}-{c} = {a - b - c}\n");
+            rand(ref a, ref b, ref c);
+            richTextBox1.AppendText($"{a}*{b}*{c} = {a * b * c}\n");
+            rand(ref a, ref b, ref c);
+            richTextBox1.AppendText($"{a}+{b}+{c} = {a + b + c}\n");
+
+            rand(ref ad, ref bd, ref cd);
+            richTextBox1.AppendText($"{ad}-{bd}-{cd} = {ad - bd - cd}\n");
+            rand(ref ad, ref bd, ref cd);
+            richTextBox1.AppendText($"{ad}*{bd}*{cd} = {ad * bd * cd}\n");
+            rand(ref ad, ref bd, ref cd);
+            richTextBox1.AppendText($"{ad}+{bd}+{cd} = {ad + bd + cd}\n");
+
+            rand(ref a, ref b, ref c);
+            rand(ref ad, ref bd, ref cd);
+            if(b != 0)
+               richTextBox1.AppendText($"{a}/{b} = {(1.0*a) / b}\n");  
+            else
+                richTextBox1.AppendText($"{a}/{b} = +INF\n");
+
+            if (b != 0)
+                richTextBox1.AppendText($"{ad}/{bd} = {ad / bd}\n");
+            else
+                richTextBox1.AppendText($"{ad}/{bd} = +INF\n");
+        }
+        private double CalculateU(double x = -4.5, double y = 0.000075, double z = 84.5)
+        {
+            double absXY = Math.Abs(x - y);
+            double cubeRoot = Math.Pow(8 + Math.Pow(absXY, 2) + 1, 1.0 / 3.0);
+            double denominator = Math.Pow(x, 2) + Math.Pow(y, 2) + 2;
+            double firstPart = cubeRoot / denominator;
+
+            double secondPart = Math.Exp(absXY) * Math.Pow(Math.Pow(Math.Tan(z), 2) + 1, x);
+
+            return firstPart - secondPart;
+        }
+        private int rand()
+        {
+             return random.Next(1, 101);
+        }
+        private void rand(ref int a_, ref int b_, ref int c_)
+        {
+            a_ = random.Next(0, 101);
+            b_ = random.Next(0, 101);
+            c_ = random.Next(0, 101);
+        }
+        private void rand(ref double a_, ref double b_, ref double c_)
+        {
+            a_ = random.Next(0, 101) / 10.0 + 0.05;
+            b_ = random.Next(0, 101) / 10.0 + 0.05;
+            c_ = random.Next(0, 101) / 10.0 + 0.05;
+        }
+        private void buttonСalculate_Click(object sender, EventArgs e)
+        {
+            string xValue = textBoxX.Text;
+            string yValue = textBoxY.Text;
+            string zValue = textBoxZ.Text;
+
+            if (string.IsNullOrEmpty(xValue) ||
+                    string.IsNullOrEmpty(yValue) ||
+                    string.IsNullOrEmpty(zValue))
+            {
+                textBox1.Text = CalculateU().ToString();
+                textBoxX.Text = "-4.5";
+                textBoxY.Text = "0.000075";
+                textBoxZ.Text = "84.5";
+            }
+            else
+            {
+                double x = Convert.ToDouble(xValue), y = Convert.ToDouble(yValue), z = Convert.ToDouble(zValue);
+                textBox1.Text = CalculateU(x, y, z).ToString();
+            }
+
+
+
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+        }
+    }
+}
